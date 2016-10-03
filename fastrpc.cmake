56ce7624cfa94a6acaa0fac726cc0555ae6e2791
Larry Wang <wxiaodong@gmail.com>
2016-10-03 16:39:34 -0700

    rebase to the latest

--- fastrpc.cmake
+++ fastrpc.cmake
@@ -12,7 +12,7 @@
 #    notice, this list of conditions and the following disclaimer in
 #    the documentation and/or other materials provided with the
 #    distribution.
-# 3. Neither the name ATLFLight nor the names of its contributors may be
+# 3. Neither the name PX4 nor the names of its contributors may be
 #    used to endorse or promote products derived from this software
 #    without specific prior written permission.
 #
@@ -89,12 +89,26 @@ include_directories(
 function(FASTRPC_STUB_GEN IDLFILE)
 	get_filename_component(FASTRPC_IDL_NAME ${IDLFILE} NAME_WE)
 	get_filename_component(FASTRPC_IDL_PATH ${IDLFILE} ABSOLUTE)
+	set (IDLINCS ${ARGN})
+    
+	# prepend -I in front of QAIC include dirs
+	set(QAIC_INCLUDE_DIRS)
+	foreach(inc ${IDLINCS})
+		string(SUBSTRING ${inc} 0 1 absolute_path_character)
+		if (absolute_path_character STREQUAL "/")
+		    	list(APPEND QAIC_INCLUDE_DIRS -I${inc})
+			message("QAIC include directory: -I${inc}")
+		else()
+	    		list(APPEND QAIC_INCLUDE_DIRS -I${CMAKE_CURRENT_SOURCE_DIR}/${inc})
+			message("QAIC include directory: -I${CMAKE_CURRENT_SOURCE_DIR}/${inc}")
+		endif()
+	endforeach()		    
 
 	# Run the IDL compiler to generate the stubs
 	add_custom_command(
 		OUTPUT ${FASTRPC_IDL_NAME}.h ${FASTRPC_IDL_NAME}_skel.c ${FASTRPC_IDL_NAME}_stub.c
 		DEPENDS ${FASTRPC_IDL_PATH}
-		COMMAND "${HEXAGON_SDK_ROOT}/tools/qaic/Ubuntu14/qaic" "-mdll" "-I" "${HEXAGON_SDK_ROOT}/${SDKINC}/stddef" "${FASTRPC_IDL_PATH}"
+		COMMAND "${HEXAGON_SDK_ROOT}/tools/qaic/Ubuntu14/qaic" "-mdll" "-I" "${HEXAGON_SDK_ROOT}/${SDKINC}/stddef" ${QAIC_INCLUDE_DIRS} ${FASTRPC_IDL_PATH}
 		WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
 		)
 
