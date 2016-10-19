a991ea6e8f5f3c2e59274a5212d4d2b22238e748
Ramakrishna Kintada <rkintada@gmail.com>
2016-10-19 16:25:57 -0700

    Removed the helper function in qurt_lib.cmake after discussing with Mark.
    Signed-off-by: Ramakrishna Kintada <rkintada@gmail.com>

--- qurt_lib.cmake
+++ qurt_lib.cmake
@@ -58,40 +58,6 @@ include(fastrpc)
 
 include (CMakeParseArguments)
 
-#function to build qurt lib with no fastrpc dependency.
-function (QURT_LIB_STANDALONE)
-	set(options)
-	set( oneValueArgs LIB_NAME )
-	set( multiValueArgs SOURCES LINK_LIBS INCS FLAGS )
-	cmake_parse_arguments(QURT_LIB_STANDALONE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
-	if( "${QURT_LIB_STANDALONE_LIB_NAME}" STREQUAL "" )
-		message( FATAL_ERROR "QURT_LIB_STANDALONE called without a target name" )
-	endif()
-
-	message( "QURT_LIB_STANDALONE_LIB_NAME = ${QURT_LIB_STANDALONE_LIB_NAME}" )
-
-	include_directories(
-		${CMAKE_CURRENT_BINARY_DIR}
-		${FASTRPC_DSP_INCLUDES}
-		)
-
-	add_library( ${QURT_LIB_STANDALONE_LIB_NAME} SHARED ${QURT_LIB_STANDALONE_SOURCES} )
-	set_property(TARGET ${QURT_LIB_STANDALONE_LIB_NAME} PROPERTY POSITION_INDEPENDENT_CODE TRUE)
-
-  if(NOT ${QURT_LIB_STANDALONE_LIBS} STREQUAL "" )
-		target_link_libraries( ${QURT_LIB_STANDALONE_LIB_NAME} ${QURT_LIB_STANDALONE_LIBS} )
-	endif()
-
-	target_include_directories(${QURT_LIB_STANDALONE_LIB_NAME} PUBLIC
-	   $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/>
-	   $<INSTALL_INTERFACE:include>)
-
-	target_include_directories(${QURT_LIB_STANDALONE_LIB_NAME} PUBLIC
-	   $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/../include/>
-	   $<INSTALL_INTERFACE:include>)
-
-endfunction()
-
 # Process DSP files
 function (QURT_LIB)
 	set(options)
